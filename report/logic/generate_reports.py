a87e3ca60e9749f157ca6f64921a3b731eceeaa7
Adam <adamjawor@gmail.com>
2024-07-22 14:14:13 +0200

    fix generate function

--- report/logic/generate_reports.py
+++ report/logic/generate_reports.py
@@ -1,6 +1,3 @@
-import io
-import json
-
 import pandas as pd
 
 from report.routers.get_data import (
@@ -13,10 +10,33 @@ from report.utils.utils import (
     convert_data_to_data_frame,
     convert_data_to_int,
     convert_data_to_str,
+    make_empty_personal_workload_report,
+    make_personal_workload_report_with_data,
 )
 
 
 def generate_teachers_reports():
+    """
+    Generates a report of teachers' workload based on the data from all_teachers, all_lectures, and all_exercises.
+    Returns:
+        df_report (DataFrame): A DataFrame containing the following columns:
+            - id (int): The teacher's ID.
+            - teacher (str): The teacher's full name.
+            - subject (str): The subject's name.
+            - hours_lectures (int): The total hours of lectures.
+            - hours_exercises (int): The total hours of exercises.
+            - study_year (int): The study year.
+            - semester (int): The semester.
+    Note:
+        - The function uses the get_all_teachers, get_all_lectures, and get_all_exercises functions to retrieve the data.
+        - The data is converted to DataFrame format using the convert_data_to_data_frame function.
+        - The lectures and exercises data are merged using the pd.merge function.
+        - The "teacher" column is created by concatenating the "name_x" and "last_name" columns.
+        - The columns to be included in the report are specified in the columns_to_move_to_report list.
+        - The DataFrame is renamed using the rename function.
+        - The study_year, semester, hours_lectures, and hours_exercises columns are converted to integer format using the convert_data_to_int function.
+        - The subject column is converted to string format using the convert_data_to_str function.
+    """
     all_teachers = get_all_teachers()
     all_lectures = get_all_lectures()
     all_exercises = get_all_exercises()
@@ -56,91 +76,48 @@ def generate_teachers_reports():
 
 
 def generate_personal_workload_reports(teacher_id: int):
-    try:
-        teacher = get_teacher(teacher_id)
-        all_lectures = get_all_lectures()
-        all_exercises = get_all_exercises()
-        df_lectures = pd.read_json(
-            io.StringIO(json.dumps(all_lectures)), orient="records"
-        )
-        df_exercises = pd.read_json(
-            io.StringIO(json.dumps(all_exercises)), orient="records"
-        )
-        df_lectures_teacher = df_lectures[df_lectures["teacher_id"] == teacher_id]
-        df_exercises_teacher = df_exercises[df_exercises["teacher_id"] == teacher_id]
+    """
+    Generate personal workload reports for a given teacher.
+    If teacher has no lectures or exercises, an empty report is created.
+    Args:
+        teacher_id (int): The ID of the teacher.
+    Returns:
+        pd.DataFrame: The complete personal workload report.
+    """
+    teacher = get_teacher(teacher_id)
+    all_lectures = get_all_lectures()
+    all_exercises = get_all_exercises()
+    df_lectures = convert_data_to_data_frame(all_lectures)
+    df_exercises = convert_data_to_data_frame(all_exercises)
+    df_lectures_teacher = df_lectures[df_lectures["teacher_id"] == teacher_id]
+    df_exercises_teacher = df_exercises[df_exercises["teacher_id"] == teacher_id]
 
-        if df_lectures_teacher.empty and df_exercises_teacher.empty:
-            print("No lectures or exercises found for the teacher saving empty report")
-            df_complete_report = pd.DataFrame(
-                {
-                    "id": [teacher_id],
-                    "teacher": [teacher["name"] + " " + teacher["last_name"]],
-                    "hours_semester1": [0],
-                    "hours_semester2": [0],
-                    "pensum": [0],
-                    "sum_hours": [0],
-                    "difference_pensum_sum_hours": [0],
-                }
-            )
-            return df_complete_report
-        else:
-            df_join_lectures_exercises = pd.concat(
-                [df_lectures_teacher, df_exercises_teacher]
-            )
-            df_semesters = df_join_lectures_exercises.groupby(
-                "semester", as_index=False
-            ).sum()
-            df_complete_report = pd.DataFrame(
-                {
-                    "id": [teacher_id],
-                    "teacher": [teacher["name"] + " " + teacher["last_name"]],
-                    "hours_semester1": [
-                        df_semesters["sum_lectures_hours"][0]
-                        + df_semesters["sum_exercises_hours"][0]
-                    ],
-                    "hours_semester2": [
-                        df_semesters["sum_lectures_hours"][1]
-                        + df_semesters["sum_exercises_hours"][1]
-                    ],
-                    "pensum": [240],
-                }
-            )
-            df_complete_report["sum_hours"] = (
-                df_complete_report["hours_semester1"]
-                + df_complete_report["hours_semester2"]
-            )
-            df_complete_report["difference_pensum_sum_hours"] = (
-                df_complete_report["pensum"] - df_complete_report["sum_hours"]
-            )
-            return df_complete_report
-    except Exception as error:
-        print(f"Generate personal workload report ERROR --- {error}")
+    if df_lectures_teacher.empty and df_exercises_teacher.empty:
+        print("No lectures or exercises found for the teacher saving empty report")
+        df_complete_report = make_empty_personal_workload_report(teacher_id, teacher)
+        return df_complete_report
+    else:
+        df_complete_report = make_personal_workload_report_with_data(
+            teacher_id, teacher, df_lectures_teacher, df_exercises_teacher
+        )
+        return df_complete_report
 
 
 def generate_summary_reports():
-    try:
-        all_teachers = get_all_teachers()
-        all_lectures = get_all_lectures()
-        all_exercises = get_all_exercises()
-        df_teachers = pd.read_json(
-            io.StringIO(json.dumps(all_teachers)), orient="records"
-        )
-        df_lectures = pd.read_json(
-            io.StringIO(json.dumps(all_lectures)), orient="records"
-        )
-        df_exercises = pd.read_json(
-            io.StringIO(json.dumps(all_exercises)), orient="records"
-        )
-        df_concat_exercises_lectures = pd.concat([df_lectures, df_exercises])
-        df_merge_with_teachers = pd.merge(
-            df_concat_exercises_lectures,
-            df_teachers,
-            how="left",
-            left_on="teacher_id",
-            right_on="id",
-        ).sort_values(by="name_x")
-        # TODO need to fixed  to prepare report that validate with database model
-        print(f"Generate summary report --- {df_merge_with_teachers}")
-        return df_merge_with_teachers
-    except Exception as error:
-        print(f"Generate summary report ERROR--- {error}")
+    all_teachers = get_all_teachers()
+    all_lectures = get_all_lectures()
+    all_exercises = get_all_exercises()
+    df_teachers = convert_data_to_data_frame(all_teachers)
+    df_lectures = convert_data_to_data_frame(all_lectures)
+    df_exercises = convert_data_to_data_frame(all_exercises)
+    df_concat_exercises_lectures = pd.concat([df_lectures, df_exercises])
+    df_merge_with_teachers = pd.merge(
+        df_concat_exercises_lectures,
+        df_teachers,
+        how="left",
+        left_on="teacher_id",
+        right_on="id",
+    ).sort_values(by="name_x")
+    # TODO need to fixed  to prepare report that validate with database model
+    print(f"Generate summary report --- {df_merge_with_teachers}")
+    return df_merge_with_teachers
--- report/utils/utils.py
+++ report/utils/utils.py
@@ -5,7 +5,7 @@ import re
 import pandas as pd
 
 
-def find_teacher_id_in_body(body):
+def find_teacher_id_in_body(body) -> int:
     """
     Find and extract the teacher ID from the given body.
     """
@@ -40,3 +40,68 @@ def convert_data_to_data_frame(data):
     Convert data from faculty service to pandas data frame.
     """
     return pd.read_json(io.StringIO(json.dumps(data)), orient="records")
+
+
+def make_empty_personal_workload_report(teacher_id: int, teacher: dict) -> pd.DataFrame:
+    """
+    Make empty personal workload report as data frame, when
+    teacher doesn't have any lecture or exercise.
+    Parameters:
+        teacher_id (int): The ID of the teacher.
+        teacher (dict): Dictionary with teacher information.
+    Returns:
+        pd.DataFrame: DataFrame containing the empty personal workload report.
+    """
+    return pd.DataFrame(
+        {
+            "id": [teacher_id],
+            "teacher": [teacher["name"] + " " + teacher["last_name"]],
+            "hours_semester1": [0],
+            "hours_semester2": [0],
+            "pensum": [0],
+            "sum_hours": [0],
+            "difference_pensum_sum_hours": [0],
+        }
+    )
+
+
+def make_personal_workload_report_with_data(
+    teacher_id: int,
+    teacher: dict,
+    df_lectures_teacher: pd.DataFrame,
+    df_exercises_teacher: pd.DataFrame,
+) -> pd.DataFrame:
+    """
+    Make personal workload report with provided data for a teacher.
+    Parameters:
+        teacher_id (int): The ID of the teacher.
+        teacher (dict): Dictionary with teacher information.
+        df_lectures_teacher (pd.DataFrame): Pandas DataFrame of lectures for the teacher.
+        df_exercises_teacher (pd.DataFrame): Pandas DataFrame of exercises for the teacher.
+    Returns:
+        pd.DataFrame: DataFrame containing the complete personal workload report.
+    """
+    df_join_lectures_exercises = pd.concat([df_lectures_teacher, df_exercises_teacher])
+    df_semesters = df_join_lectures_exercises.groupby("semester", as_index=False).sum()
+    df_complete_report = pd.DataFrame(
+        {
+            "id": [teacher_id],
+            "teacher": [teacher["name"] + " " + teacher["last_name"]],
+            "hours_semester1": [
+                df_semesters["sum_lectures_hours"][0]
+                + df_semesters["sum_exercises_hours"][0]
+            ],
+            "hours_semester2": [
+                df_semesters["sum_lectures_hours"][1]
+                + df_semesters["sum_exercises_hours"][1]
+            ],
+            "pensum": [240],
+        }
+    )
+    df_complete_report["sum_hours"] = (
+        df_complete_report["hours_semester1"] + df_complete_report["hours_semester2"]
+    )
+    df_complete_report["difference_pensum_sum_hours"] = (
+        df_complete_report["pensum"] - df_complete_report["sum_hours"]
+    )
+    return df_complete_report
